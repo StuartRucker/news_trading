b1c34074c696d41a5fc0b9525988d3042d307f2c
stuartrucker <stuart.a.rucker@gmail.com>
2022-02-11 12:49:51 -0500

    d

--- scrape_wsj.py
+++ scrape_wsj.py
@@ -9,6 +9,7 @@ import random
 import re
 from multiprocessing import Pool
 import json
+import os
 
 def get_headers():
     return {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0'}
