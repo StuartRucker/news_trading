c5dc7f39060b0010abc977318c1e0b10144c4112
Gordon <gordonchi11@gmail.com>
2022-02-12 10:48:31 -0800

    Update create_gpt3_predictions.py

--- create_gpt3_predictions.py
+++ create_gpt3_predictions.py
@@ -6,45 +6,60 @@ import os
 import openai
 import json
 
-openai.organization = "org-GhuqPhpoi029dThqqUHRulbB"
-openai.api_key = "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"  # os.getenv("OPENAI_API_KEY")
-openai.Engine.list()
 
-# Opening JSON file
-f = open('data/wsj/2022-02-11.json')
+def generate_predictions(date):
+    openai.organization = "org-GhuqPhpoi029dThqqUHRulbB"
+    openai.api_key = "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"  # os.getenv("OPENAI_API_KEY")
+    openai.Engine.list()
 
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+    input_file = 'data/wsj-predictions/' + date + '.json'
+    output_file = 'data/wsj/' + date + '.json'
+    # Opening JSON file
+    f = open('data/wsj/2022-02-11.json')
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+
+    predictions = []
+
+    for elem in data:
+        try:
+            blurb = "List out the names and stock tickers of the companies most associated with the news?\n\n"
+            blurb += "\"Title: " + elem["title"] + " Summary:" + elem["summary"] + "\""
+
+            openai.Engine.retrieve("text-davinci-001")
+            response = openai.Completion.create(
+                engine="text-davinci-001",
+                prompt=blurb,
+                max_tokens=64,
+                temperature=0)
+
+            print("Stocks affected:", response["choices"][0]["text"])
+            stocks_affected = response["choices"][0]["text"]
+            blurb += "\n\n" + response["choices"][0]["text"]
+            blurb += "\n\nHow will the prices of each of these companies change as a result of this news? Explain."
+            response = openai.Completion.create(
+                engine="text-davinci-001",
+                prompt=blurb,
+                max_tokens=128,
+                temperature=0.2)
+
+            # print("----")
+            #
+            # print(response["choices"][0]["text"])
+            # print("-"*80)
+
+            elem["Stocks Affected"] = stocks_affected
+            elem["Prediction"] = response["choices"][0]["text"]
+            predictions.append(elem)
+
+        except:
+            print("Article is invalid for prediction.")
+
+    with open('data/wsj-predictions/2022-02-11.json', "w") as f:
+        json.dump(predictions, f)
 
 
-for elem in data:
-    try:
-        blurb = "What are the names and stock tickers of the companies most associated with the news?\n\n"
-        blurb += "\"Title: " + elem["title"] + " Summary:" + elem["summary"]+"\""
-
-        openai.Engine.retrieve("text-davinci-001")
-        response = openai.Completion.create(
-          engine="text-davinci-001",
-          prompt=blurb,
-          max_tokens=64,
-          temperature=0.3)
-
-        print("Stocks affected:", response["choices"][0]["text"])
-        blurb += "\n\n" + response["choices"][0]["text"]
-        blurb += "\n\nHow will the prices of each of these companies change as a result of this news? Explain."
-        response = openai.Completion.create(
-            engine="text-davinci-001",
-            prompt=blurb,
-            max_tokens=128,
-            temperature=0)
-
-        print("----")
-
-        print(response["choices"][0]["text"])
-        print("-"*80)
-
-    except:
-        print("Article is invalid for prediction.")
-
-
+if __name__ == '__main__':
+    generate_predictions("2022-02-11")
